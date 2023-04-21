7d222873b5319df6295687ca17f439354f653144
codaliendeepak <deepak@codalien.com>
2023-04-21 08:47:04 +0530

    challenge list

--- frontend/src/components/ChallengerList.jsx
+++ frontend/src/components/ChallengerList.jsx
@@ -1,8 +1,51 @@
 import React, { useState } from 'react'
-import Profile from './Profile'
+import ChallengeService from '../services/challenge.service';
+import Profile from './Profile';
+import { ToastContainer, toast } from 'react-toastify';
+import UserService from '../services/user.service';
 
 function ChanllenceList() {
-    const [amount, setAmount] = useState(100)
+    const [amount, setAmount] = useState(100);
+    const [openChallenges,setOpenChallenges]=useState({});
+    const [runningChallenges,setRunningChallenges]=useState({});
+    const [challenge,setChallenge]=useState({challenger:"", amount:0, contender:"", status:'', roomCode:"", game:"" });
+
+    useState(()=>{
+      async function test(){
+        try{
+            let user=await UserService.getUser();
+            if(user&&user.id){
+                console.log(user.id)
+                const userid={challenger:user.id}
+                setChallenge((challenge)=>({...challenge,...userid}));
+            }
+            let runChallenge=await ChallengeService.listChallengeByStatus('STARTED');
+            setRunningChallenges(runChallenge);
+            let openChallenge=await ChallengeService.listChallengeByStatus('CREATED');
+            setOpenChallenges(openChallenge);
+            console.log(runChallenge,openChallenge)
+        }catch(c){
+            console.log(c);
+            toast.error(c.message);
+            throw new Error(c);
+        }
+      }
+      test();
+    },[]);
+
+    async function createChallenge(){       
+        try{
+            console.log(challenge);
+            let chall=await ChallengeService.createChallenge(challenge);
+            console.log(chall);
+//````1===----------- to do list & add all challnges via web socket--==---------=--===------------   
+        }catch(c){
+            console.log(c);
+            toast.error(c.message);
+            throw new Error(c);
+        }
+    }
+
     return (
         <>
             <div className='card'>
@@ -10,9 +53,10 @@ function ChanllenceList() {
                     <h3>Create a Challenge</h3>
                 </div>
                 <div className='body section-center'>
-                    <input placeholder='Amount' className='input-white' name='amount' /> <button  className='btn-green ml10'>SET</button>
+                    <input placeholder='Amount' className='input-white' name='amount' onChange={(e)=>{let amount={amount:e.target.value}; setChallenge((challenge)=>({...challenge,...amount}))}} value={challenge.amount}/> <button onClick={createChallenge}  className='btn-green ml10'>SET</button>
                 </div>
             </div>
+            <ToastContainer />
             <div className='card'>
                 <div className='head-card'>
                     <h3>Open Challenge</h3>
@@ -20,24 +64,14 @@ function ChanllenceList() {
                 </div>
                 <div className=''>
                     <ul className='challenge-list'>
-                        <li>
-                            <div> <img className='profile-small' src='../images/profile.png' alt='Sunil Kumar' /> Sunil Kumar</div> <div className='green-text'>₹ 100</div>  <button className='btn-play'>Play</button>
-                        </li>
-                        <li>
-                            <div> <img className='profile-small' src='../images/profile.png' alt='Sunil Kumar' /> Sunil Kumar</div> <div className='green-text'>₹ 100</div>  <button className='btn-play'>Play</button>
-                        </li>
-                        <li>
-                            <div> <img className='profile-small' src='../images/profile.png' alt='Sunil Kumar' /> Sunil Kumar</div> <div className='green-text'>₹ 100</div>  <button className='btn-play'>Play</button>
-                        </li>
-                        <li>
-                            <div> <img className='profile-small' src='../images/profile.png' alt='Sunil Kumar' /> Sunil Kumar</div> <div className='green-text'>₹ 100</div>  <button className='btn-play'>Play</button>
-                        </li>
-                        <li>
-                            <div> <img className='profile-small' src='../images/profile.png' alt='Sunil Kumar' /> Sunil Kumar</div> <div className='green-text'>₹ 100</div>  <button className='btn-play'>Play</button>
-                        </li>
-                        <li>
-                            <div> <img className='profile-small' src='../images/profile.png' alt='Sunil Kumar' /> Sunil Kumar</div> <div className='green-text'>₹ 100</div>  <button className='btn-play'>Play</button>
-                        </li>
+                        
+                            {openChallenges&&!!openChallenges.length?openChallenges.map((item)=>{
+                            return(<li>
+                            <div> <img className='profile-small' src='../images/profile.png' alt='Sunil Kumar' /> {item.challenger.name}</div> <div className='green-text'>₹ {item.amount}</div>  <button className='btn-play'>Play</button>
+                            </li>)
+                            }):"LOADING"}
+                       
+                       
                     </ul>
 
                 </div>
@@ -49,27 +83,11 @@ function ChanllenceList() {
                 </div>
                 <div className=''>
                     <ul className='challenge-list running'>
-                        <li>
-                            <div className='direction'><img className='profile-small' src='../images/profile.png' alt='Sunil Kumar' /> Sunil Kumar</div> <div className='green-text direction'><img src='../images/vs.png' alt='vice-versa' /> ₹ 100</div>  <div className='direction'> <img className='profile-small' src='../images/profile.png' alt='Sunil Kumar' /> Sunil Kumar</div>
-                        </li>
-                        <li>
-                            <div className='direction'><img className='profile-small' src='../images/profile.png' alt='Sunil Kumar' /> Sunil Kumar</div> <div className='green-text direction'><img src='../images/vs.png' alt='vice-versa' /> ₹ 100</div>  <div className='direction'> <img className='profile-small' src='../images/profile.png' alt='Sunil Kumar' /> Sunil Kumar</div>
-                        </li>
-                        <li>
-                            <div className='direction'><img className='profile-small' src='../images/profile.png' alt='Sunil Kumar' /> Sunil Kumar</div> <div className='green-text direction'><img src='../images/vs.png' alt='vice-versa' /> ₹ 100</div>  <div className='direction'> <img className='profile-small' src='../images/profile.png' alt='Sunil Kumar' /> Sunil Kumar</div>
-                        </li>
-                        <li>
-                            <div className='direction'><img className='profile-small' src='../images/profile.png' alt='Sunil Kumar' /> Sunil Kumar</div> <div className='green-text direction'><img src='../images/vs.png' alt='vice-versa' /> ₹ 100</div>  <div className='direction'> <img className='profile-small' src='../images/profile.png' alt='Sunil Kumar' /> Sunil Kumar</div>
-                        </li>
-                        <li>
-                            <div className='direction'><img className='profile-small' src='../images/profile.png' alt='Sunil Kumar' /> Sunil Kumar</div> <div className='green-text direction'><img src='../images/vs.png' alt='vice-versa' /> ₹ 100</div>  <div className='direction'> <img className='profile-small' src='../images/profile.png' alt='Sunil Kumar' /> Sunil Kumar</div>
-                        </li>
-                        <li>
-                            <div className='direction'><img className='profile-small' src='../images/profile.png' alt='Sunil Kumar' /> Sunil Kumar</div> <div className='green-text direction'><img src='../images/vs.png' alt='vice-versa' /> ₹ 100</div>  <div className='direction'> <img className='profile-small' src='../images/profile.png' alt='Sunil Kumar' /> Sunil Kumar</div>
-                        </li>
-                        <li>
-                            <div className='direction'><img className='profile-small' src='../images/profile.png' alt='Sunil Kumar' /> Sunil Kumar</div> <div className='green-text direction'><img src='../images/vs.png' alt='vice-versa' /> ₹ 100</div>  <div className='direction'> <img className='profile-small' src='../images/profile.png' alt='Sunil Kumar' /> Sunil Kumar</div>
-                        </li>
+                    {(runningChallenges&&!!runningChallenges.length)?runningChallenges.map((item)=>{
+                            return(<li>
+                            <div> <img className='profile-small' src='../images/profile.png' alt='Sunil Kumar' /> {item.challenger.name}</div> <div className='green-text'>₹ {item.amount}</div>  <button className='btn-play'>Play</button>
+                            </li>)
+                            }):"LOADING"}
                     </ul>
 
                 </div>
---
+++ frontend/src/services/challenge.service.js
@@ -0,0 +1,121 @@
+import ApiCoreService from "./api.core.service";
+
+
+export default class ChallengeService extends ApiCoreService{
+
+    static async listAllChallenge(){
+        try {
+            return await this.graphCall('withAuth.listChallenge', `
+            {
+                withAuth(token:"auto"){
+                  listChallenge{
+                    id
+                    amount
+                    game {
+                      id
+                      name
+                      status
+                    }
+                    contender{
+                      name
+                      id
+                      phones{
+                        number
+                        countryCode
+                      }
+                    }
+                    status
+                    roomCode
+                    challenger{
+                      id
+                      name
+                      phones{
+                        countryCode
+                        number
+                      }
+                    }
+                    meta
+                    winner{
+                      name
+                      id
+                      phones{
+                        countryCode
+                        number
+                      }
+                    }    
+                  }
+                }
+              }`,{});
+        }catch(c){
+            console.log(c);
+            throw new Error('Unable to list Challenge');
+        }
+    }
+
+    static async listChallengeByStatus(status){
+      //to do check if valid status-------------
+      try {
+        return await this.graphCall('withAuth.listChallenge', `
+        {
+          withAuth(token:"auto"){
+            listChallenge(criteria:"{\\"status\\":\\"${status}\\"}"){
+              id
+              challenger{
+                id
+                name
+              }
+              contender{
+                id
+                name
+              }
+              amount
+              roomCode
+              game{
+                id
+                name
+              }
+              winner{
+                id
+                name
+              }
+              status
+            }
+          }
+        }
+        `,{});
+    }catch(c){
+        console.log(c);
+        throw new Error('Unable to list Challenge');
+    }
+    }
+
+    static async createChallenge(challenge){
+        try {
+            return await this.graphCall('mutationWithAuth.fabricate.createChallenge', `
+            mutation{
+                mutationWithAuth(token:"auto"){
+                  fabricate{
+                    createChallenge(challenger:"${challenge.challenger}" amount:${challenge.amount} contender:"643e502e746d5ecbaa4936b8" status:PENDING roomCode:"213" game:"64413054d74babfdb353e6b0" ){
+                      id
+                      challenger{
+                        id
+                        name
+                      }
+                      contender{
+                        id
+                        name
+                      }
+                      roomCode
+                      status
+                      amount
+                    }
+                  }
+                }
+              }`,{});
+        }catch(c){
+            console.log(c);
+            throw new Error('Unable to create Challenge');
+        }
+    }
+
+}
