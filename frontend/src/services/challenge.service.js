92d83f61facd075d4298d2427fd2002527c8eec0
Umar <umar@umar.com>
2023-05-19 17:35:02 +0530

    show low balance.

--- frontend/src/services/challenge.service.js
+++ frontend/src/services/challenge.service.js
@@ -47,8 +47,8 @@ export default class ChallengeService extends ApiCoreService{
                 }
               }`,{});
         }catch(c){
-            //console.log(c);
-            throw new Error('Unable to list Challenge');
+            //console.log(c);'Unable to list Challenge'
+            throw new Error(c);
         }
     }
 
@@ -84,8 +84,8 @@ export default class ChallengeService extends ApiCoreService{
         }
         `,{});
     }catch(c){
-        //console.log(c);
-        throw new Error('Unable to list Challenge');
+        //console.log(c);'Unable to list Challenge'
+        throw new Error(c);
     }
     }
 
@@ -113,8 +113,7 @@ export default class ChallengeService extends ApiCoreService{
                 }
               }`,{});
         }catch(c){
-            //console.log(c);
-            throw new Error('Unable to create Challenge');
+            throw new Error(c);
         }
     }
 
@@ -151,7 +150,8 @@ export default class ChallengeService extends ApiCoreService{
         }`,{});
     }catch(c){
        //console.log(c);
-        throw new Error('Unable to update Challenge');
+       //'Unable to update Challenge'
+        throw new Error(c);
     }
     }
 }
